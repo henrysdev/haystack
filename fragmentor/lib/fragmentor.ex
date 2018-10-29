23486c265601d9ab0b406cfb4de3b4c233a46783
henrysdev <henrysdev@gmail.com>
2018-10-29 10:45:14 -0500

    init project

---
+++ fragmentor/lib/fragmentor.ex
@@ -0,0 +1,18 @@
+defmodule Fragmentor do
+  @moduledoc """
+  Documentation for Fragmentor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fragmentor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ fragmentor/test/fragmentor_test.exs
@@ -0,0 +1,8 @@
+defmodule FragmentorTest do
+  use ExUnit.Case
+  doctest Fragmentor
+
+  test "greets the world" do
+    assert Fragmentor.hello() == :world
+  end
+end
