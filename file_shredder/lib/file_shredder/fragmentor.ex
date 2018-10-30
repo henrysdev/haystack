62418a64df0dbf9bcd972f2aa75f18ddfa949786
Henry Warren <henrysdev@gmail.com>
2018-10-30 13:43:50 -0500

    cleaned up code + broke parallel functionality into its own module

--- file_shredder/lib/file_shredder/fragmentor.ex
+++ file_shredder/lib/file_shredder/fragmentor.ex
@@ -13,7 +13,7 @@ defmodule FileShredder.Fragmentor do
 
   """
 
-  # high-order function for generating an
+  # high-order functions for generating an
   # anonymous function for fragment partitioning
   defp lazy_chunking(n) do
     fn
@@ -27,68 +27,39 @@ defmodule FileShredder.Fragmentor do
   end
 
   defp lazy_cleanup do
-    fn
-      []  -> {:cont, []}
-      acc -> {:cont, acc, []}
-    end
-  end
-
-    
-  defp spawn_worker(chunk_of_work, function) do
-    Task.async(fn -> Enum.map(chunk_of_work, function) end)
-  end
-
-  defp join_worker(chunk_of_work) do
-    Task.await(chunk_of_work)
-  end
-
-  def pmap(collection, process_count, function) do
-    coll_size  = Enum.count(collection)
-    chunk_size = Integer.floor_div(coll_size, process_count)
-    IO.inspect collection
-    collection
-      |> Enum.chunk_every(chunk_size)
-      |> Enum.map(&(spawn_worker(&1, function)))
-      |> Enum.map(&(join_worker(&1)))
-      |> Enum.concat()
+    fn acc -> {:cont, acc, []} end
   end
 
   defp add_encr({chunk, seq_id}, hashkey) do
-    {FileShredder.CryptoUtils.encrypt(chunk, hashkey), seq_id}
+    { FileShredder.CryptoUtils.encrypt(chunk, hashkey), seq_id }
   end
   
-  defp add_hmac(chunk, hashkey, seq_id) do
-    {chunk <> FileShredder.CryptoUtils.gen_hmac(hashkey, seq_id), seq_id}
+  defp add_hmac({chunk, seq_id}, hashkey) do
+    { chunk <> FileShredder.CryptoUtils.gen_hmac(hashkey, seq_id), seq_id } 
   end
 
-  defp write_out({fragment, _seq_id}) do
-    {:ok, file} = File.open "debug/out/#{:rand.uniform(999)}.frg", [:write]
+  defp write_out({fragment, _}) do
+    { :ok, file } = File.open "debug/out/#{:rand.uniform(999)}.frg", [:write]
     IO.binwrite file, fragment
     File.close file
   end
 
-  defp work(fragment, password, hashkey) do
-    fragment
+  defp work({chunk, seq_id}, hashkey) do
+    { chunk, seq_id }
     |> add_encr(hashkey)
-    |> add_hmac(password)
+    |> add_hmac(hashkey)
     |> write_out()
   end
-
 
   def fragment(file_path, n, password) do
     %{ size: file_size } = File.stat! file_path
     chunk_size = Integer.floor_div(file_size, n)
     hashkey = FileShredder.CryptoUtils.gen_key(password)
-    frags = file_path
+    file_path
     |> File.stream!([], chunk_size)
-    |> Stream.with_index() # add sequence IDs
-    |> Stream.chunk_while([], lazy_chunking(n), lazy_cleanup()) # ensure exactly n fragments
-    # vvv entirely parallelizable vvv
-    #pmap(frags, 2, fn frag -> work(frag, password, hashkey) end)
-    |> Stream.map(fn frag -> add_encr(frag, hashkey) end) # add encryption
-    |> Stream.map(fn {frag, seq_id} -> add_hmac(frag, hashkey, seq_id) end) # add hmac
-    |> Stream.each(fn chunk -> write_out(chunk) end) # write out
-    |> Enum.to_list()
+    |> Stream.with_index()
+    |> Stream.chunk_while([], lazy_chunking(n), lazy_cleanup())
+    |> FileShredder.ParallelUtils.pmap(&work(&1, hashkey))
   end
 
 end
---
+++ file_shredder/lib/file_shredder/parallel_utils.ex
@@ -0,0 +1,9 @@
+defmodule FileShredder.ParallelUtils do
+  
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
+  end
+
+end
