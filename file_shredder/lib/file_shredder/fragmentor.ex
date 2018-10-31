211253c122143892d400cb749149aef075596d10
henrysdev <henrysdev@gmail.com>
2018-10-31 17:30:21 -0500

    yet another refactor / cleaner approach to lazy fragmentation of n padded parts

--- file_shredder/lib/file_shredder/fragmentor.ex
+++ file_shredder/lib/file_shredder/fragmentor.ex
@@ -13,24 +13,23 @@ defmodule FileShredder.Fragmentor do
 
   """
 
-  # high-order functions for generating an
-  # anonymous function for fragment partitioning
-
-  defp lazy_pad({chunk, 0}), do: {chunk, 0}
-  defp lazy_pad({chunk, p}) do
-    { chunk <> to_string(:string.chars(0, p)), p }
+  defp pad_frag(chunk, chunk_size) do
+    pad_amt = chunk_size - byte_size(chunk)
+    pad_bytes = to_string(:string.chars(0, pad_amt))
+    chunk = chunk <> pad_bytes
+    {chunk, pad_amt}
   end
 
-  # def fragment(file_path, n, password) do
-  #   %{ size: file_size } = File.stat! file_path
-  #   chunk_size = Integer.floor_div(file_size, n)
-  #   hashkey = Utils.Crypto.gen_key(password)
-  #   file_path
-  #   |> File.stream!([], chunk_size)
-  #   |> Stream.with_index()
-  #   |> Stream.chunk_while([], lazy_chunking(n), lazy_cleanup())
-  #   |> Utils.Parallel.pmap(&finish_frag(&1, hashkey))
-  # end
+  defp dummy(chunk_size) do
+    pad_amt = 0
+    chunk = to_string(:string.chars(0, chunk_size))
+    {chunk, pad_amt}
+  end
+
+  defp gen_dummies(0, _chunk_size), do: []
+  defp gen_dummies(dummy_count, chunk_size) do
+    for _ <- 0..dummy_count, do: dummy(chunk_size)
+  end
 
   def fragment(file_path, n, password) do
     hashkey = Utils.Crypto.gen_key(password)
@@ -41,36 +40,36 @@ defmodule FileShredder.Fragmentor do
     partial_pad = rem(padding, chunk_size)
     dummy_count = div((padding - partial_pad), chunk_size)
 
-    IO.inspect n, label: "n"
-    IO.inspect file_size, label: "file_size"
-    IO.inspect chunk_size, label: "chunk_size"
-    IO.inspect padding, label: "padding"
-    IO.inspect partial_pad, label: "partial_pad"
-    IO.inspect dummy_count, label: "dummy_count"
+    IO.inspect dummy_count
 
     file_path
     |> File.stream!([], chunk_size)
-    |> Stream.map(&{&1, chunk_size - String.length(&1)})
-    |> Stream.map(&(lazy_pad(&1)))
-    #|> Stream.concat(Stream.take(n-1), partial_pad(Stream.take(-1)))
-    #|> Stream.map() # fill in bytes of last elem
-    #|> Stream.concat() # concat new dummy pad frags
-    |> Enum.to_list()
+    |> Stream.map(&(pad_frag(&1, chunk_size))) # add pad_amt
+    |> Stream.concat(gen_dummies(dummy_count, chunk_size))
+    |> Stream.with_index() # add seq_id
+    |> Utils.Parallel.pmap(&finish_frag(&1, hashkey))
+
   end
 
-  defp finish_frag({chunk, seq_id}, hashkey) do
-    { chunk, seq_id }
+  defp finish_frag({ {chunk, seq_id}, pad}, hashkey) do
+    { chunk, seq_id, pad }
     |> add_encr(hashkey)
     |> add_hmac(hashkey)
+    |> serialize()
     |> write_out()
   end
 
-  defp add_encr({chunk, seq_id}, hashkey) do
-    { Utils.Crypto.encrypt(chunk, hashkey), seq_id }
+  defp add_encr({chunk, seq_id, pad}, hashkey) do
+    { Utils.Crypto.encrypt(chunk, hashkey), seq_id, pad }
   end
   
-  defp add_hmac({chunk, seq_id}, hashkey) do
-    chunk <> Utils.Crypto.gen_hmac(hashkey, seq_id)
+  defp add_hmac({chunk, seq_id, pad}, hashkey) do
+    { chunk, Utils.Crypto.gen_hmac(hashkey, seq_id), pad }
+  end
+
+  defp serialize({chunk, hmac, pad}) do
+    # TODO bake pad_amt into encrypted payload (serialize twice...?)
+    chunk <> hmac
   end
 
   defp write_out(fragment) do
--- file_shredder/lib/utils/parallel.ex
+++ file_shredder/lib/utils/parallel.ex
@@ -1,7 +1,6 @@
 defmodule Utils.Parallel do
   
   def pmap(collection, func) do
-    IO.inspect collection
     collection
     |> Enum.map(&(Task.async(fn -> func.(&1) end)))
     |> Enum.map(&Task.await/1)
