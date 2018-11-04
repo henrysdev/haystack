0883812ff01dad7ba48f1b6bb28a7bc516f36126
henrysdev <henrysdev@gmail.com>
2018-11-04 12:51:01 -0600

    added fields file_size and file_name to fragment + hefty progress on reassemble

--- file_shredder/lib/file_shredder/fragmentor.ex
+++ file_shredder/lib/file_shredder/fragmentor.ex
@@ -15,8 +15,7 @@ defmodule FileShredder.Fragmentor do
 
   defp pad_frag(chunk, chunk_size) do
     pad_amt = chunk_size - byte_size(chunk)
-    pad_bytes = to_string(:string.chars(0, pad_amt))
-    chunk = chunk <> pad_bytes
+    chunk = chunk <> to_string(:string.chars(0, pad_amt))
     {chunk, pad_amt}
   end
 
@@ -31,8 +30,11 @@ defmodule FileShredder.Fragmentor do
     for _ <- 0..dummy_count, do: dummy(chunk_size)
   end
 
+  defp add_file_size
+
   def fragment(file_path, n, password) do
     hashkey = Utils.Crypto.gen_key(password)
+    file_name = Path.basename(file_path)
     %{ size: file_size } = File.stat! file_path
 
     chunk_size = Float.ceil(file_size/n) |> trunc()
@@ -42,52 +44,71 @@ defmodule FileShredder.Fragmentor do
 
     file_path
     |> File.stream!([], chunk_size)
-    |> Stream.map(&(pad_frag(&1, chunk_size))) # add pad_amt
+    |> Stream.map(&pad_frag(&1, chunk_size)) # pad frags + add pad_amt
     |> Stream.concat(gen_dummies(dummy_count, chunk_size)) # add dummy frags
+    |> Stream.map(&Tuple.append(&1, file_name)) # add file_size
+    |> Stream.map(&Tuple.append(&1, file_size)) # add file_size
     |> Stream.with_index() # add sequence IDs
     #|> Enum.map(&finish_frag(&1, hashkey))
     |> Utils.Parallel.pmap(&finish_frag(&1, hashkey))
 
   end
 
-  defp finish_frag({ { payload, pad_amt }, seq_id }, hashkey) do
-    { payload, pad_amt }
+  defp finish_frag({ { payload, pad_amt, file_name, file_size }, seq_id }, hashkey) do
+    { payload, pad_amt, file_name, file_size }
     |> encr_payload(hashkey)
     |> encr_pad_amt(hashkey)
+    |> encr_file_name(hashkey)
+    |> encr_file_size(hashkey)
     |> add_seq_hash(hashkey, seq_id)
     |> add_hmac(hashkey)
     |> serialize()
     |> write_out()
   end
 
-  defp encr_payload({ payload, pad_amt }, hashkey) do
-    { Utils.Crypto.encrypt(payload, hashkey), pad_amt }
+  defp encr_payload({ payload, pad_amt, file_name, file_size }, hashkey) do
+    payload = Utils.Crypto.encrypt(payload, hashkey)
+    { payload, pad_amt, file_name, file_size }
   end
 
-  defp encr_pad_amt({ payload, pad_amt }, hashkey) do
-    { payload, Utils.Crypto.encrypt(Integer.to_string(pad_amt), hashkey) }
+  defp encr_pad_amt({ payload, pad_amt, file_name, file_size }, hashkey) do
+    pad_amt = Utils.Crypto.encrypt(Integer.to_string(pad_amt), hashkey)
+    { payload, pad_amt, file_name, file_size }
+  end
+
+   defp encr_file_name({ payload, pad_amt, file_name, file_size }, hashkey) do
+    file_name = Utils.Crypto.encrypt(file_name, hashkey)
+    { payload, pad_amt, file_name, file_size}
   end
   
-  defp add_seq_hash({ payload, pad_amt }, hashkey, seq_id) do
-    { payload, pad_amt, Utils.Crypto.gen_multi_hash([hashkey, seq_id]) }
+  defp encr_file_size({ payload, pad_amt, file_name, file_size }, hashkey) do
+    file_size = Utils.Crypto.encrypt(Integer.to_string(file_size), hashkey)
+    { payload, pad_amt, file_name, file_size}
+  end
+  
+  defp add_seq_hash({ payload, pad_amt, file_name, file_size }, hashkey, seq_id) do
+    seq_hash = Utils.Crypto.gen_multi_hash([hashkey, seq_id])
+    { payload, pad_amt, file_name, file_size, seq_hash }
   end
 
-  defp add_hmac({ payload, pad_amt, seq_hash }, hashkey) do
-    hmac = Utils.Crypto.gen_multi_hash([payload, pad_amt, seq_hash, hashkey])
-    { payload, pad_amt, seq_hash, hmac }
+  defp add_hmac({ payload, pad_amt, file_name, file_size, seq_hash }, hashkey) do
+    hmac = Utils.Crypto.gen_multi_hash([payload, pad_amt, file_size, seq_hash, hashkey])
+    { payload, pad_amt, file_name, file_size, seq_hash, hmac }
   end
 
-  defp serialize({ payload, pad_amt, seq_hash, hmac }) do
+  defp serialize({ payload, pad_amt, file_name, file_size, seq_hash, hmac }) do
     Poison.encode!(%{
-      "payload"  => payload,
-      "pad_amt"  => pad_amt,
-      "seq_hash" => seq_hash,
-      "hmac"     => hmac,
+      "payload"   => payload,
+      "pad_amt"   => pad_amt,
+      "file_name" => file_name,
+      "file_size" => file_size,
+      "seq_hash"  => seq_hash,
+      "hmac"      => hmac,
     })
   end
 
   defp write_out(fragment) do
-    { :ok, file } = File.open "debug/out/#{:rand.uniform(5)}.frg", [:write]
+    { :ok, file } = File.open "debug/out/#{:rand.uniform(4096)}.json", [:write]
     IO.binwrite file, fragment
     File.close file
   end
--- file_shredder/lib/file_shredder/reassembler.ex
+++ file_shredder/lib/file_shredder/reassembler.ex
@@ -12,12 +12,56 @@ defmodule FileShredder.Reassembler do
       :world
 
   """
-  def reassemble(dirpath, password) do
-    hmac_paths = Path.wildcard(dirpath)
-  end
-
-  defp deserialize(fragment, password) do
+  defp deserialize(fragment) do
     Poison.Parser.parse!(fragment)
   end
+
+  defp valid_hmac?(fragment, hashkey) do
+    hmac_parts = [
+      Map.get(fragment, "payload"),
+      Map.get(fragment, "pad_amt"),
+      Map.get(fragment, "file_size"),
+      Map.get(fragment, "seq_hash"),
+      hashkey
+    ]
+    # verify integrity of hmac
+    Map.get(fragment, "hmac") == Utils.Crypto.gen_multi_hash(hmac_parts)
+  end
+
+  defp gen_seq_map(fragment, acc) do
+    Map.put(acc, Map.get(fragment, "seq_hash"), fragment)
+  end
+
+  def reassemble(dirpath, password) do
+    hashkey = Utils.Crypto.gen_key(password)
+
+    seq_map = Path.wildcard(dirpath)
+    |> Stream.map(&File.read!(&1))
+    |> Stream.map(&deserialize(&1)) # deserialize json fragment
+    |> Stream.filter(&valid_hmac?(&1, hashkey)) # verify integrity
+    |> Enum.reduce(%{}, &gen_seq_map(&1, &2)) # reduce into sequence map
+
+    first_fragment = Map.get(seq_map, gen_seq_hash(0, hashkey))
+    file_size = Utils.Crypto.decrypt(Map.get(first_fragment, "file_size"))
+    alloc_buffer_file("target_file.txt")
+    # [0..map_size(seq_map)-1]
+    # |> gen_seq_hash(hashkey)
+    # |> 
+    #next_seq = gen_seq_hash(0, hashkey)
+    #Map.has_key?(seq_map, next_seq)
+    #init_frag = Map.get(seq_map, )
+
+    #|> IO.inspect
+    #|> Stream.map(&integrity_check(&1))
+    #|> Enum.to_list()
+  end
+
+  defp gen_seq_hash(seq_id, hashkey) do
+    seq_hash = Utils.Crypto.gen_multi_hash([hashkey, seq_id])
+  end
+
+  defp alloc_buffer_file()
+
+  
 
 end
