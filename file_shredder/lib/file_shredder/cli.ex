23f1df63f9b6b3464566453d6c163ac019060d0d
henrysdev <henrysdev@gmail.com>
2018-11-27 00:07:06 -0600

    built out CLI to include basic options including using a keyfile vs a user entered password

--- file_shredder/lib/file_shredder/cli.ex
+++ file_shredder/lib/file_shredder/cli.ex
@@ -4,8 +4,7 @@ defmodule FileShredder.CLI do
     #
     # [ Fragmentation ]
     # fragment -in <string/filepath> -fragcount <integer> -keyfile <path/to/keyfile> -out <string/dirpath>
-    # fragment -in <string/filepath> -fragcount <integer> -genkey <boolean>          -out <string/dirpath>
-    # fragment -in <string/filepath> -fragcount <integer> -password <string>         -out <string/dirpath>
+    # fragment -in <string/filepath> -fragcount <integer> -password <boolean>        -out <string/dirpath>
     #
     # [ Reassembly ]
     # reassemble -in <string/dirpath> -keyfile <path/to/keyfile> -out <string/dirpath>
@@ -19,14 +18,47 @@ defmodule FileShredder.CLI do
     end
 
     defp parse_args(argv) do
-      switches = [in: :string]
-      aliases  = [in: :in]
+      switches = [in: :string, shards: :integer, out: :string, keyfile: :string, password: :boolean]
+      aliases  = [i: :in, c: :shards, o: :out, k: :keyfile]
       parse = OptionParser.parse(argv, switches: switches, aliases: aliases)
       case parse do
-        {opts, ["fragment"], _} -> IO.inspect opts
-        {opts, ["reassemble"], _} -> IO.inspect opts
+        {opts, ["fragment"], _}   -> map_params(opts, :fragment)
+        {opts, ["reassemble"], _} -> map_params(opts, :reassemble)
         _ -> IO.puts "Invalid parameters"
       end
+    end
+
+    defp map_params(opts, :fragment) do
+      case opts[:password] do
+        true -> FileShredder.fragment(
+          opts[:in],
+          opts[:shards],
+          pword_prompt()
+        )
+        nil  -> FileShredder.fragment(
+          opts[:in],
+          opts[:shards],
+          "pword" # TODO: parse key file here!
+        )
+      end
+    end
+
+    defp map_params(opts, :reassemble) do
+      case opts[:password] do
+        true -> FileShredder.reassemble(
+          opts[:in],
+          pword_prompt()
+        )
+        nil  -> FileShredder.reassemble(
+          opts[:in],
+          "pword" # TODO: parse key file here!
+        )
+      end
+    end
+
+    defp pword_prompt() do
+      password = IO.gets "Enter a password: "
+      password |> String.trim()
     end
 
 end
