dd7a891ada6d1f5223cee802a92224224c2922d8
henrysdev <henrysdev@gmail.com>
2018-11-28 23:25:47 -0600

    improved help dialogue + better password loop

--- file_shredder/lib/file_shredder/cli.ex
+++ file_shredder/lib/file_shredder/cli.ex
@@ -45,12 +45,14 @@ defmodule FileShredder.CLI do
       nil -> FileShredder.fragment(
         opts[:in], # 
         opts[:count],
-        pword_prompt()
+        password_get("Enter password: "), #pword_prompt(),
+        opts[:out]
       )
       _  -> FileShredder.fragment(
         opts[:in],
         opts[:count],
-        opts[:keyfile] |> Utils.File.parse_keyfile()
+        opts[:keyfile] |> Utils.File.parse_keyfile(),
+        opts[:out]
       )
     end
   end
@@ -60,7 +62,8 @@ defmodule FileShredder.CLI do
     case opts[:keyfile] do
       nil -> FileShredder.reassemble(
         opts[:in],
-        pword_prompt(),
+        password_get("Enter password: "),
+        #pword_prompt(),
         opts[:out]
       )
       _  -> FileShredder.reassemble(
@@ -71,37 +74,49 @@ defmodule FileShredder.CLI do
     end
   end
 
-  defp pword_prompt() do
-    IO.write "Enter password: "
-    pword1 = :io.get_password()
-    IO.write "Confirm password: "
-    pword2 = :io.get_password()
-    case pword1 == pword2 do
-      true  -> pword1
-      _     -> pword_prompt(:retry)
-    end
+  # https://github.com/hexpm/hex/blob/5dd8ae020dc4c31bc662e490c79221423fc5d9f6/lib/mix/tasks/hex/util.ex#L46-L74
+  # Password prompt that hides input by every 1ms
+  # clearing the line with stderr
+  def password_get(prompt) do
+    pid = spawn_link fn -> loop(prompt) end
+    ref = make_ref()
+
+    value = IO.gets(prompt <> " ")
+
+    send pid, {:done, self(), ref}
+    receive do: ({:done, pid, ref}  -> :ok)
+
+    value
   end
-  defp pword_prompt(:retry) do
-    IO.puts "Passwords do not match. Try again"
-    pword_prompt()
+
+  defp loop(prompt) do
+    receive do
+      {:done, parent, ref} ->
+        send parent, {:done, self, ref}
+        IO.write :standard_error, "\e[2K\r"
+    after
+      1 ->
+        IO.write :standard_error, "\e[2K\r#{prompt} "
+        loop(prompt)
+    end
   end
 
   def help_dialog() do
     """
     [ Usage ]
     Fragmentation
-    fragment --in <string/filepath> --count <integer> --keyfile <path/to/keyfile> --out <string/dirpath>
-    fragment --in <string/filepath> --count <integer> --out <string/dirpath>
+    fragment --in <string> --count <integer> --keyfile <string> --out <string>
+    fragment --in <string> --count <integer> --out <string>
     
     Reassembly
-    reassemble --in <string/dirpath> --keyfile <path/to/keyfile> --out <string/dirpath>
-    reassemble --in <string/dirpath> --out <string/dirpath>
+    reassemble --in <string> --keyfile <path/to/keyfile> --out <string>
+    reassemble --in <string> --out <string>
     
     [ Parameters ]
-    --in =  path to the input file (for fragmentation) or the input directory (for reassembly).
-    --count = number of fragments to split the file into during fragmentation.
-    --keyfile = path to a file containing the plaintext password to be used for encryption (for fragmentation) or decryption(for reassembly).
-    --out = path to the output directory where fragments (for fragmentation) or the original file (for reassembly) will be located.
+    --in      | -i = path to the input file (for fragmentation) or the input directory (for reassembly).
+    --count   | -c = number of fragments to split the file into during fragmentation.
+    --keyfile | -k = path to a file containing the plaintext password to be used for encryption (for fragmentation) or decryption(for reassembly).
+    --out     | -o = path to the output directory where fragments (for fragmentation) or the original file (for reassembly) will be located.
     """ |> IO.puts()
   end
 
