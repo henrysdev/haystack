642e3d3d45db31a215f087cf01f5cc293d463193
Henry Warren <henrysdev@gmail.com>
2018-12-05 23:33:42 -0600

    fixed application name

--- haystack/mix.exs
+++ haystack/mix.exs
@@ -3,7 +3,7 @@ defmodule Haystack.MixProject do
 
   def project do
     [
-      app: :safe_split,
+      app: :haystack,
       version: "0.1.0",
       elixir: "~> 1.7",
       escript: escript_config(),
@@ -31,8 +31,6 @@ defmodule Haystack.MixProject do
     [
       {:exprof, "~> 0.2.0"},
       {:poolboy, "~> 1.5.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
